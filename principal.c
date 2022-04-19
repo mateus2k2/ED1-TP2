74387ef4b189a6607dbea99f1e78ac51a3d64b12
Mateus Filipe <mateusfilipe2k2@gmail.com>
2022-04-19 14:40:25 -0300

    ORDEM

---
+++ principal.c
@@ -0,0 +1,22 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+/*
+gcc -c ordenacao.c -Wall
+gcc -c principal.c -Wall
+gcc ordenacao.o principal.o -o exe
+
+gcc *.c -o exe -Wall
+./exe
+*/
+
+int main(){
+
+
+
+
+    return 0;
+}
